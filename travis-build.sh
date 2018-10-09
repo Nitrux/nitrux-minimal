7e4cc69266b440b4b6a682fde7db358cca648204
UriHerrera <hello@uriherrera.com>
2018-10-08 23:23:03 -0500

    first commit

---
+++ travis-build.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+apt-get --yes update
+apt-get --yes install wget equivs curl git
+
+cd package/
+
+DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
+
+GIT_COMMIT=$(git rev-parse --short HEAD)
+
+echo "
+Section: misc
+Priority: optional
+Homepage: https://nxos.org
+
+Package: nitrux-minimal
+Version: 0.1.0-${GIT_COMMIT}
+Maintainer: Uri Herrera <uri_herrera@nxos.org>
+Depends: $DEPENDS
+Architecture: amd64
+Description: Minimal Core of Nitrux.
+" > configuration
+
+equivs-build configuration
