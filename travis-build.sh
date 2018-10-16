9b54c74c9f36d100afce60565059ae12d3de59ce
UriHerrera <hello@uriherrera.com>
2018-10-16 04:57:14 -0500

    bump version

--- travis-build.sh
+++ travis-build.sh
@@ -15,7 +15,7 @@ Priority: optional
 Homepage: https://nxos.org
 
 Package: nitrux-minimal
-Version: 0.1.0-${GIT_COMMIT}
+Version: 0.1.1-${GIT_COMMIT}
 Maintainer: Uri Herrera <uri_herrera@nxos.org>
 Depends: $DEPENDS
 Architecture: amd64
