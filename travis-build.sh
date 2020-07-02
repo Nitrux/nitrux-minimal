01d1d89fdc71fa1c93d8c8fb5fb20906a0d6f806
Uri Herrera <hello@uriherrera.com>
2020-07-02 15:09:46 -0500

    add dep

--- travis-build.sh
+++ travis-build.sh
@@ -15,7 +15,7 @@ Priority: optional
 Homepage: https://nxos.org
 
 Package: nitrux-minimal-legacy
-Version: 0.1.17-${GIT_COMMIT}
+Version: 0.1.18-${GIT_COMMIT}
 Maintainer: Uri Herrera <uri_herrera@nxos.org>
 Depends: $DEPENDS
 Architecture: amd64
