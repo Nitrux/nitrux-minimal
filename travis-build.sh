ae7576f3c54563b524801025ca0ae22e4c86adb1
Uri Herrera <hello@uriherrera.com>
2020-08-15 22:07:31 -0500

    typo

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@
 apt-get --yes update
 apt-get --yes install wget equivs curl git
 
-deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' packages/dependencies | paste -sd ,)
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
 git_commit=$(git rev-parse --short HEAD)
 
 > configuration printf "%s\n" \
