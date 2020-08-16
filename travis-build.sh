4e6e0a2f9cabc3c72f3dd985fe90bd03dee79869
Uri Herrera <hello@uriherrera.com>
2020-08-15 22:10:07 -0500

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@
 apt-get --yes update
 apt-get --yes install wget equivs curl git
 
-deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' dependencies | paste -sd ,)
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
 git_commit=$(git rev-parse --short HEAD)
 
 > configuration printf "%s\n" \
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-minimal" \
-	"Version: 0.1.21-$git_commit" \
+	"Version: 0.1.22-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: amd64" \
