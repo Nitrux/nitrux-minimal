5fd2deb4bb152959fca95dc2c205496bacd2bc31
Uri Herrera <hello@uriherrera.com>
2021-07-09 15:06:33 -0500

    rm dep

--- travis-build.sh
+++ travis-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-minimal-arm64" \
-	"Version: 0.0.2-$git_commit" \
+	"Version: 0.0.3-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nitrux-minimal, nitrux-minimal-legacy" \
