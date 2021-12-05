edd0f7a8dda839acb5ab12b86defa272fa94102b
UriHerrera <hello@uriherrera.com>
2021-12-05 02:35:50 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-minimal-legacy" \
-	"Version: 0.2.8-$git_commit" \
+	"Version: 0.2.9-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nitrux-minimal" \
